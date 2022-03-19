8ceaa01431f8886cb0fd655f59e8eac3d4766b1c
Nhung Doan <98629735+rosedoan@users.noreply.github.com>
2022-03-18 23:09:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -91,3 +91,77 @@ searchForm.addEventListener("submit", handleSubmit);
 
 let currentWeather = document.querySelector(".fa-location-dot");
 currentWeather.addEventListener("click", displayCurrentWeather);
+
+//Show Temperature of cities in heading
+function hanoiTemperature(response) {
+  console.log(response.data.main);
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function showHanoiTemperature(event) {
+  event.preventDefault();
+  let apiKey = "577c46134c672fc2d3022ec30b750312";
+  let city = "hanoi";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(hanoiTemperature);
+}
+
+function berlinTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function showBerlinTemperature(event) {
+  event.preventDefault();
+  let apiKey = "577c46134c672fc2d3022ec30b750312";
+  let city = "berlin";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(berlinTemperature);
+}
+
+function tokyoTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function showTokyoTemperature(event) {
+  event.preventDefault();
+  let apiKey = "577c46134c672fc2d3022ec30b750312";
+  let city = "tokyo";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(tokyoTemperature);
+}
+
+function seoulTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function showSeoulTemperature(event) {
+  event.preventDefault();
+  let apiKey = "577c46134c672fc2d3022ec30b750312";
+  let city = "seoul";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(tokyoTemperature);
+}
+
+let hanoi = document.querySelector("#hanoi");
+hanoi.addEventListener("click", showHanoiTemperature);
+
+let berlin = document.querySelector("#berlin");
+berlin.addEventListener("click", showBerlinTemperature);
+
+let tokyo = document.querySelector("#tokyo");
+tokyo.addEventListener("click", showTokyoTemperature);
+
+let seoul = document.querySelector("#seoul");
+seoul.addEventListener("click", showSeoulTemperature);
